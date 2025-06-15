8746c8184cb12ffc489c402713b3fe0444841e4d
raposoG <gabriel.henrique7@hotmail.com>
2025-06-15 04:30:12 -0300

    Add RootLayout component for main application structure with internationalization support

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Edu_NSW_ACT_Foundation, Inter } from "next/font/google";
+import { NextIntlClientProvider } from "next-intl";
+import { getLocale } from "next-intl/server";
+
+const edu_nsw_act_hand = Edu_NSW_ACT_Foundation({
+  subsets: ["latin"],
+  variable: "--font-edu-nsw-act-hand",
+});
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Raposo Dev",
+  description: "Personal portfolio of Raposo Dev",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const locale = await getLocale();
+
+  return (
+    <html lang={`${locale}`}>
+      <body className={`${edu_nsw_act_hand.variable} antialiased font-sans ${inter.className}`}>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
